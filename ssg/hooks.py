536e8a2f8327e3d54f9c02d2bc1cdae527cfa20c
Dan Shield <Dan.Shield@allstate.com>
2020-12-28 08:43:38 -0600

    More issues

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -9,7 +9,7 @@ def register(hook, order = 0):
 
 def event(hook, *args):
     for order in sorted(_callbacks.get(hook, {})):
-        for func in _callbacks[hook][order]
+        for func in _callbacks[hook][order]:
             func(*args)
 
 def filter(hook, value, *args):
