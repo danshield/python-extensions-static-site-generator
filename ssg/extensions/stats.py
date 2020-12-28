39672ba75d9aeb32a377fe4d6ac77b4bd5f99c06
Dan Shield <Dan.Shield@allstate.com>
2020-12-28 12:09:06 -0600

    Fixes

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -2,20 +2,22 @@ from ssg import hooks
 import time
 
 start_time = None
-total_writtn = 0
+total_written = 0
 
 @hooks.register("start_build")
 def start_build():
-    global start_time = time.localtime()
+    global start_time
+    start_time = time.time()
 
-@hooks.register("total_written")
+@hooks.register("written")
 def written():
-    global total_written += 1
+    global total_written
+    total_written = total_written + 1
 
 @hooks.register("stats")
 def stats():
-    final_time = time.localtime() - start_time
-    average = final_time / total_written if total_written > 0 else 0
+    final_time = time.time() - start_time
+    average = final_time / total_written if total_written else 0
 
-    report = "Converted: {} - Time: {:.2f} sec - Avg: {:.4f} sec/file"
+    report = "Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file"
     report.format(total_written, final_time, average)
