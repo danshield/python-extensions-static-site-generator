35ee3cbbccc75e1baa4b76f74a45dfe49fb6b47b
Dan Shield <Dan.Shield@allstate.com>
2020-12-28 08:31:06 -0600

    Module 2

---
+++ ssg/hooks.py
@@ -0,0 +1,18 @@
+_callbacks = {}
+
+def register(name, order = 0)
+    def register_callback(func):
+        _callbacks.setdefault(order, []).append(func)
+
+    return register_callback
+
+def event(hook, *args):
+    for order in sorted(_callbacks.get(hook, {})):
+        func(*args)
+
+def filter(hook, value, *args):
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
+            value = func(value, *args)
+
+    return value
